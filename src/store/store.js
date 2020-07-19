a6682257fcf03e40cfc5ef252a938f1300feb68c
Abdou <abdou.g12dz@gmail.com>
2020-07-19 13:47:36 +0100

    Adding redux devtools ext

--- src/store/store.js
+++ src/store/store.js
@@ -1,19 +1,23 @@
-import {createStore, combineReducers} from 'redux';
+import { createStore, combineReducers } from 'redux';
 import todosReducer from './todosReducer';
-import {persistReducer} from 'redux-persist';
+import { persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 
 
 const reducers = {
-    todos : todosReducer,
+    todos: todosReducer,
 };
 const persistConfig = {
     key: 'root',
     storage,
-    stateReconciler : autoMergeLevel2,
+    stateReconciler: autoMergeLevel2,
 };
 const rootReducer = combineReducers(reducers);
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const configureStore = () => createStore(persistedReducer);
+export const configureStore = () => createStore(
+    persistedReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+);
